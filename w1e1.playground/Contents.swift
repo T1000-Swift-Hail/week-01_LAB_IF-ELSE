8b0f774cd563d5ccd1561217eae7182208a13897
Ahmed Alenazi <ahmedalenazi@MacBook-Air-alkhas-b-Ahmed.local>
2021-10-05 11:13:07 +0300

    done exirsis

---
+++ w1e1.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+
+
+
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorPicked = "Red"
+if (colorPicked == colorBlue) {
+    print("Not the picked color!")
+} else if (colorPicked == colorRed) {
+    print("Yes! this is the picked color")
+}
+
+else {
+    print( "color picked is not known")
+}
+
+
+
+
+
