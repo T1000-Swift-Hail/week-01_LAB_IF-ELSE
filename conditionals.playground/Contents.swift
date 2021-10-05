24a6a37a62d0e3541dd404f4918583f7c709a4f0
Hind Alharbi <hindalharbi@Hinds-MacBook-Pro.local>
2021-10-05 11:17:38 +0300

    This is LAB_IF-ELSE

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorPicked = "bird"
+
+if (colorPicked == "Blue"){
+    
+  print("Not the picked color!")
+
+}
+else if (colorPicked == "Red"){
+    
+    print ("Yes! this is the picked color")
+}
+else {
+    print("color picked is not known")
+}
