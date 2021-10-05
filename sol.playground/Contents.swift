8988c72992bee0c529188c0ee26bfb23e8268320
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-05 11:31:59 +0300

    sol

--- sol.playground/Contents.swift
+++ sol.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 let colorBlue = "Blue"
 let colorPicked = "Red"
-let colorRed = "Red"
+let colorRed = "pink"
 
 if (colorPicked == "Blue"){
     print("Not the picked color!")
@@ -11,6 +11,17 @@ else if (colorPicked == "Red"){
     print("Yes! this is the picked color")
 }
 else{
-    print("color picked is not Known")
+    print("color picked is not known")
 }
 
+
+
+
+
+
+//let samecolor = ("\(colorPicked),\(colorRed)")
+//print(samecolor)
+//
+//let sol = samecolor == "Blue" ? "Yes! this is the picked color" : "color picked is not known"
+//
+//print(sol)
