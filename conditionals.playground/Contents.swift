aee7e80e8206bb11a6f4e807cf08b2b8754126dc
Huda N S <hnsa5@MacBook-Air-Huda.local>
2021-10-05 11:32:33 +0300

    I finished the solution

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let (colorBlue) : String = "Blue"
+let (colorRed) : String = "Red"
+let (colorPicked) : String = "bird"
+
+if (colorPicked == "Blue") {
+    print ("not the picked color !")
+}
+else if (colorPicked == colorRed) {
+    print ("yes this is the picked color")
+}
+else {
+    print ("color picked is not known")
+}
+
+
