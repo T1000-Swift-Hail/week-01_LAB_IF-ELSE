2a58be648206a9d0820a94ed3f8de34aff7e4acb
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-10-05 11:50:31 +0300

    finish lab

---
+++ hw lab IF .playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+let coloreBlue = "Blue"
+let coloreRed = "Red"
+let colorePicked = "Red"
+if (colorePicked == "Blue"){
+    print( "Not the picked color!")
+} else if (colorePicked == "Red") {
+    
+    print("yes! this is the picked colore")
+}
