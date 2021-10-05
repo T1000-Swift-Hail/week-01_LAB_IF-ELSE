ea039e434e6159eb4edf19ab88c45830f1042671
Njoud Alrshidi <njoudalrshidi@MacBook-Pro-Njoud.local>
2021-10-05 11:26:17 +0300

    DoneLabIfElse

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let colorBlue =  "Blue"
+let colorRed =  "Red"
+let colorpicked =  "brid"
+
+
+if (colorpicked == "blue"){
+    
+    print("Not the picked colore!")
+}
+else if (colorpicked == "Red"){
+    print("Yes! this is the picked color ")
+}
+else{
+    
+    print("color picked is not know")
+    
+    
+}
