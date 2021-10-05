60efa17112d10cbd916f776bebc3c75efed2a082
mac <mac@MacBook-Pro-mac.local>
2021-10-05 11:32:21 +0300

    Done_LAB_IF_ELS

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let colorBlue: String = "Blue"
+let colorRed: String = "Red"
+let colorPicked: String = "bird"
+
+if (colorPicked == "Blue"){
+    print("Not the picked color!")
+}
+
+else if(colorPicked == "Red"){
+    print("Yes! this is the picked color")
+}
+
+else{
+    print("color picked is not known")
+}
+
+
