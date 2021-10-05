e63dac5bf1cda8206a44908abc85de9d9d4a9455
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-05 11:36:13 +0300

    finished the HW LAB

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let colorBlue: String = "blue"
+let colorRed: String = "Red"
+let colorPicked: String = "Red"
+
+if (colorPicked == "colorBlue" ) {
+    
+    print("Not the Picked color!")
+    }
+
+else if (colorPicked == "colorRed"){
+
+            print("Yes! This is The picked color")
+} else {
+    
+    print("color Picked is not known")
+}
+
+
+
+
