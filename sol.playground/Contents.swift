cb2476328a2bf692fcad59d7ea017b5051d2607e
Monafh <monafh@MacBook-Pro-alkhas-b-Monafh.local>
2021-10-05 11:03:32 +0300

    sol

---
+++ sol.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let colorBlue = "Blue"
+let colorPicked = "Red"
+let colorRed = "Red"
+
+if (colorPicked == "Blue"){
+    print("Not the picked color!")
+}
+else if (colorPicked == "Red"){
+    print("Yes! this is the picked color")
+}
+else{
+    print("color picked is not Known")
+}
+
