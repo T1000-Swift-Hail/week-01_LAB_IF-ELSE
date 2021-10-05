7be597705fb701f96ff2b1e1413049c277a5bc21
Seham الشطنان <sehamalshtnan@Sehams-MacBook-Air.local>
2021-10-05 11:42:39 +0300

    homework done

---
+++ conditionalse.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorpicked = "Bird"
+
+if (colorpicked == "Blue"){
+    print("Not the picked color!")
+}
+else if (colorpicked  == "Red"){
+    
+    print ("yes! this is the picked color")
+}
+else {
+    ("colorpicked is not knwon")
+    
+}
+
