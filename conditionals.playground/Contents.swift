84a72345dcd1d34f4de0a5fec24d0e27b4df1d48
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-05 12:06:36 +0300

    homork done

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorpicked = "bird"
+
+if (colorpicked == "Blue") {
+    print("Not the picked color!")
+           }
+
+else if (colorpicked == "Red") {
+        print("yes! this is the picked color")
+    }
+
+else  {
+        print("colorpicked is not knwon")
+    }
+   
+   
+           
+           
+
+
+
+
