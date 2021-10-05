8be17a951d540f502149ba9683dc2cca138f46ce
user <user@MacBook-Air-.local>
2021-10-05 11:02:49 +0300

    done homework

---
+++ conditional.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let colorBlue = "Blue"
+let colorRed = "Red"
+//let colorPicked = "Red"
+let colorPicked = "bird"
+if (colorPicked == "blue ") {
+    print("Not the picked color!")
+} else if (colorPicked == "Red") {
+    print("Yes! this is the picked color")
+
+}
+else {
+    print("color picked is not known")
+}
