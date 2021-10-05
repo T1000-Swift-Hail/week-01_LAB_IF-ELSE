ffc58426940d4372d0c75e0b3582f47ad0eb09ea
hanaalmutlak <Hana.Khamis.has.s@tuwaiq.edu.sa>
2021-10-05 11:54:05 +0300

    Done lab

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorpicked = "bird"
+if (colorpicked == "Blue"){
+    print ("Not the packed color!")
+} else if(colorpicked == "Red") {
+print ("Yes!this is the picked color")
+}else {
+    print ("color picked is not known")
+    
+}
+
+
+            
+
+    
+    
+    
+   
+
