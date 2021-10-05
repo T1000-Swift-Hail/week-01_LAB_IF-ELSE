98e825eca5e3a69de64c7d0894c463ffa2fe4ead
طلال عبيدالله دعيع القلادي <talalalglaadi@MacBook-Air-tlal.local>
2021-10-05 11:59:12 +0300

    exercise Done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorPicked = "Red"
+
+if (colorPicked == "Blue" )
+
+{
+    print("Not the picked color !")}
+
+else if (colorPicked == "Red")
+
+{ print("Yes! this is the picked color")}
+
+else
+{print("color picked is not known")}
+
