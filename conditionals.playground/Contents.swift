85a0c4bcbd05cc83e08aa938ab7522e06e9464fd
hesah <hesah.raja.has.s@tuwaiq.edu.sa>
2021-10-05 11:25:22 +0300

    done conditionals

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+let colorBlue = "Blue"
+let colorRed = "Red"
+let colorPicked = "bird"
+
+if (colorPicked == "Blue"){
+    print ("Not the picked color!")
+}
+else if (colorPicked == "Red") {
+    
+    print ("Yes! this is the picked color")
+    
+}
+else {
+    
+    print ("color picked is not known")
+}
+
+
+
