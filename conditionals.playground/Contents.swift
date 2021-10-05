a2a0f36abc6869621cdf655570a6ddb56ecaa53a
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-10-05 11:39:21 +0300

    Finish

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let colorBlue  : String = "Blue"
+let colorRed  :String = "Red"
+var colorPicked : String = "brid"
+
+if  (colorPicked == "Blue"){
+    print(" Not the picked color!")}
+    
+else if  (colorPicked == "Red"){
+    print(" Yes! this is the picked color  ")
+    
+}
+
+else {
+    print ("color picked is not known")
+}
+
