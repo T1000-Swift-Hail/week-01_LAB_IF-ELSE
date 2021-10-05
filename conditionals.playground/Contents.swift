af1822e66617a79c602c94456d382078f4efadb6
Mastorah <mas@Mastorahs-MacBook-Air.local>
2021-10-05 11:43:43 +0300

    finish homework

---
+++ conditionals.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let colorBlue :String = "Blue"
+
+let colorRed :String = "Red"
+let colorpicked :String = "bird"
+
+if colorpicked == ("Blue"){
+    print("Not the picked color!")
+}  else if colorpicked==("Red"){
+    print("Yes! this is the picked color")
+    
+
+} else{
+    print("color picked is not known")
+    
+}
